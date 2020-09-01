979f8b47e1f9fa5a81ce489e5ed5fd1b590e3af6
IrynaPiatrova <irina_petrova759@mail.ru>
2020-09-01 23:17:14 +0300

    Update second solution

--- src/parseTheStringTask/ParseTheStringTask_v2.java
+++ src/parseTheStringTask/ParseTheStringTask_v2.java
@@ -18,52 +18,61 @@ public class ParseTheStringTask_v2 {
         StringBuilder str = new StringBuilder();
 
         int indexFirstParenthesis =
-                getIndex(stringWithoutParenthesisOnTheSides, ParseTheStringTaskConstants.OPEN_PARENTHESIS);
+                stringWithoutParenthesisOnTheSides.indexOf(ParseTheStringTaskConstants.OPEN_PARENTHESIS);
         int indexLastParenthesis =
                 stringWithoutParenthesisOnTheSides.lastIndexOf(ParseTheStringTaskConstants.CLOSE_PARENTHESIS);
 
-        if (indexFirstParenthesis <= 0 || indexLastParenthesis <= 0) {
-            return getResultString(stringWithoutParenthesisOnTheSides, pref);
+        if (indexFirstParenthesis < 0 || indexLastParenthesis < 0) {
+            return getResultString(stringWithoutParenthesisOnTheSides
+                    .replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR), pref);
         }
 
         //append values before first parenthesis
         str.append(stringWithoutParenthesisOnTheSides, 0, indexFirstParenthesis);
-        String keyParentValue = getKeyParentValue(str);
+
+        //get last value before first parenthesis
+        String keyParentValue = getLastValue(str);
 
         //append values after last parenthesis
         str.append(stringWithoutParenthesisOnTheSides, indexLastParenthesis + 1,
                 stringWithoutParenthesisOnTheSides.length());
 
-        List<String> values = Arrays.stream(str.toString().split(ParseTheStringTaskConstants.COMMA))
-                .map(s -> s.replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR)).sorted()
-                .collect(Collectors.toList());
-
         StringBuilder result = new StringBuilder();
-        values.forEach(strValue -> {
+
+        getSortedValues(str).forEach(strValue -> {
             result.append(getResultString(strValue, pref));
             if (keyParentValue != null && keyParentValue.equals(strValue)) {
                 String hierarchicalSubstring =
                         stringWithoutParenthesisOnTheSides.substring(indexFirstParenthesis + 1, indexLastParenthesis);
-                result.append(getParseResultStringValue(hierarchicalSubstring,
-                        new StringBuilder(pref).append(ParseTheStringTaskConstants.DASH_STR).toString()));
+                result.append(getParseResultStringValue(hierarchicalSubstring, getNewPref(pref)));
             }
         });
 
         return result.toString();
     }
 
-    private static String getKeyParentValue(StringBuilder str) {
+    private static List<String> getSortedValues(StringBuilder str) {
+        return Arrays.stream(str.toString().split(ParseTheStringTaskConstants.COMMA))
+                .map(s -> s.replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR))
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private static String getNewPref(String pref) {
+        return new StringBuilder(pref)
+                .append(ParseTheStringTaskConstants.DASH_STR)
+                .toString();
+    }
+
+    private static String getLastValue(StringBuilder str) {
         String[] split = str.toString().split(ParseTheStringTaskConstants.COMMA);
         return split[split.length - 1];
     }
 
-    private static int getIndex(String stringWithoutParenthesis, String str) {
-        return stringWithoutParenthesis.indexOf(str);
-    }
-
     private static String getResultString(String stringWithoutParenthesis, String pref) {
-        return new StringBuilder().append(pref).append(ParseTheStringTaskConstants.STR_SPACE)
-                .append(stringWithoutParenthesis.replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR))
+        return new StringBuilder(pref)
+                .append(ParseTheStringTaskConstants.STR_SPACE)
+                .append(stringWithoutParenthesis)
                 .append(System.getProperty("line.separator")).toString();
     }
 }
