b1cc8ccacbbd9c0d4b71be1191b4f196fa15738c
IrynaPiatrova <irina_petrova759@mail.ru>
2020-09-03 00:52:53 +0300

    Update second solution

---
+++ src/parseTheStringTask/ParseTheStringTaskService.java
@@ -0,0 +1,65 @@
+package parseTheStringTask;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ParseTheStringTaskService {
+
+    public static String getStringWithoutParenthesisOnTheSides(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+    public static List<String> getSortedValues(StringBuilder str) {
+        return Arrays.stream(str.toString()
+                .split(ParseTheStringTaskConstants.COMMA))
+                .map(removeSpaces())
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static Function<String, String> removeSpaces() {
+        return s -> s.replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR);
+    }
+
+    public static String getNewPref(String pref) {
+        return new StringBuilder(pref).append(ParseTheStringTaskConstants.DASH_STR).toString();
+    }
+
+    public static String getLastValue(StringBuilder str) {
+        String[] split = str.toString().split(ParseTheStringTaskConstants.COMMA);
+        return split[split.length - 1];
+    }
+
+    public static String getResultString(String stringWithoutParenthesis, String pref) {
+        return new StringBuilder(pref)
+                .append(ParseTheStringTaskConstants.STR_SPACE)
+                .append(stringWithoutParenthesis)
+                .append(System.getProperty("line.separator")).toString();
+    }
+
+    public static int getClosingParenthesisPosition(String str, int indexFirstParenthesis) {
+        char[] chars = str.toCharArray();
+        int counter = 0;
+
+        if (indexFirstParenthesis < 0) {
+            return -1;
+        }
+
+        for (int i = indexFirstParenthesis; i <= chars.length; i++) {
+            if (chars[i] == '(') {
+                counter++;
+            }
+
+            if (chars[i] == ')') {
+                counter--;
+            }
+
+            if (counter == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
--- src/parseTheStringTask/ParseTheStringTask_v2.java
+++ src/parseTheStringTask/ParseTheStringTask_v2.java
@@ -1,16 +1,11 @@
 package parseTheStringTask;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class ParseTheStringTask_v2 {
 
     public static void main(String[] args) {
         System.out.println("Result : ");
         String stringWithoutParenthesisOnTheSides =
-                ParseTheStringTaskConstants.STR.substring(1, ParseTheStringTaskConstants.STR.length() - 1);
+                ParseTheStringTaskService.getStringWithoutParenthesisOnTheSides(ParseTheStringTaskConstants.STR);
         System.out.println(
                 getParseResultStringValue(stringWithoutParenthesisOnTheSides, ParseTheStringTaskConstants.EMPTY_STR));
     }
@@ -20,18 +15,20 @@ public class ParseTheStringTask_v2 {
 
         int indexFirstParenthesis =
                 stringWithoutParenthesisOnTheSides.indexOf(ParseTheStringTaskConstants.OPEN_PARENTHESIS);
-        int indexClosingParenthesis =
-                getClosingParenthesisPosition(stringWithoutParenthesisOnTheSides, indexFirstParenthesis);
+        int indexClosingParenthesis = ParseTheStringTaskService
+                .getClosingParenthesisPosition(stringWithoutParenthesisOnTheSides, indexFirstParenthesis);
 
         if (indexFirstParenthesis < 0 || indexClosingParenthesis < 0) {
-            return getResultString(removeSpaces().apply(stringWithoutParenthesisOnTheSides), pref);
+            return ParseTheStringTaskService
+                    .getResultString(ParseTheStringTaskService.removeSpaces().apply(stringWithoutParenthesisOnTheSides),
+                            pref);
         }
 
         //append values before first parenthesis
         str.append(stringWithoutParenthesisOnTheSides, 0, indexFirstParenthesis);
 
         //get last value before first parenthesis
-        String keyParentValue = getLastValue(str);
+        String keyParentValue = ParseTheStringTaskService.getLastValue(str);
 
         //append values after last parenthesis
         str.append(stringWithoutParenthesisOnTheSides, indexClosingParenthesis + 1,
@@ -39,62 +36,16 @@ public class ParseTheStringTask_v2 {
 
         StringBuilder result = new StringBuilder();
 
-        getSortedValues(str).forEach(strValue -> {
-            result.append(getResultString(strValue, pref));
+        ParseTheStringTaskService.getSortedValues(str).forEach(strValue -> {
+            result.append(ParseTheStringTaskService.getResultString(strValue, pref));
             if (keyParentValue != null && keyParentValue.equals(strValue)) {
-                String hierarchicalSubstring =
-                        stringWithoutParenthesisOnTheSides.substring(indexFirstParenthesis + 1, indexClosingParenthesis);
-                result.append(getParseResultStringValue(hierarchicalSubstring, getNewPref(pref)));
+                String hierarchicalSubstring = stringWithoutParenthesisOnTheSides
+                        .substring(indexFirstParenthesis + 1, indexClosingParenthesis);
+                result.append(
+                        getParseResultStringValue(hierarchicalSubstring, ParseTheStringTaskService.getNewPref(pref)));
             }
         });
 
         return result.toString();
-    }
-
-    private static List<String> getSortedValues(StringBuilder str) {
-        return Arrays.stream(str.toString().split(ParseTheStringTaskConstants.COMMA)).map(removeSpaces()).sorted()
-                .collect(Collectors.toList());
-    }
-
-    private static Function<String, String> removeSpaces() {
-        return s -> s.replaceAll("\\s", ParseTheStringTaskConstants.EMPTY_STR);
-    }
-
-    private static String getNewPref(String pref) {
-        return new StringBuilder(pref).append(ParseTheStringTaskConstants.DASH_STR).toString();
-    }
-
-    private static String getLastValue(StringBuilder str) {
-        String[] split = str.toString().split(ParseTheStringTaskConstants.COMMA);
-        return split[split.length - 1];
-    }
-
-    private static String getResultString(String stringWithoutParenthesis, String pref) {
-        return new StringBuilder(pref).append(ParseTheStringTaskConstants.STR_SPACE).append(stringWithoutParenthesis)
-                .append(System.getProperty("line.separator")).toString();
-    }
-
-    private static int getClosingParenthesisPosition(String str, int indexFirstParenthesis) {
-        char[] chars = str.toCharArray();
-        int counter = 0;
-
-        if (indexFirstParenthesis < 0) {
-            return -1;
-        }
-
-        for (int i = indexFirstParenthesis; i <= chars.length; i++) {
-            if (chars[i] == '(') {
-                counter++;
-            }
-
-            if (chars[i] == ')') {
-                counter--;
-            }
-
-            if (counter == 0) {
-                return i;
-            }
-        }
-        return -1;
     }
 }
