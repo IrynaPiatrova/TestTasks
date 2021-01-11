645c2692b5ef269bc448b9a0767f7e7bd25e7c83
Irina Mitskevich <irina_petrova759@mail.ru>
2021-01-11 13:59:41 +0300

    Test ParameterizedClass

---
+++ src/udemy/java/blackbelt/generics/ParameterizedClass.java
@@ -0,0 +1,26 @@
+package udemy.java.blackbelt.generics;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        Info<String> stringInfo = new Info<>("hi!");
+        System.out.println(stringInfo);
+
+        Info<Integer> integerInfo = new Info<>(1);
+        System.out.println(integerInfo);
+    }
+}
+
+class Info<T> { // can use <V>, <V1, V2> instead of <T>
+    private T value; //can't be static
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "value=" + value +
+                '}';
+    }
+}
